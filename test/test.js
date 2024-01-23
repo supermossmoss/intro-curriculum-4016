5a00f6126392cadbc27f7448ca82e2143dee37a6
ryota <ryota6114@gmail.com>
2024-01-23 11:32:35 +0900

    /logout のリダイレクトの挙動の実装

--- test/test.js
+++ test/test.js
@@ -29,3 +29,9 @@ describe('/login', () => {
       .expect(200);
   });
 });
+
+describe('/logout', () => {
+  test('/ にリダイレクトされる', async () => {
+    await request(app).get('/logout').expect('Location', '/').expect(302);
+  });
+});
